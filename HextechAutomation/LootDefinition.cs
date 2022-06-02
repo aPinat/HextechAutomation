f6582d64f9a83cae29b32e72f0785b9da5856e00
aPinat <apinat@pinat.dev>
2022-06-02 03:45:44 +0200

    Add LootDefinition type

---
+++ HextechAutomation/LootDefinition.cs
@@ -0,0 +1,33 @@
+﻿namespace HextechAutomation;
+
+public record LootDefinition(LootItemList LootItemList, RecipeList RecipeList, QueryResult QueryResult, PlayerLoot[] PlayerLoot);
+
+public record LootItemList(LootItem[] LootItems, long LastUpdate);
+
+public record LootItem(string LootName,
+    string? Asset,
+    string Type,
+    string Rarity,
+    long Value,
+    long? StoreItemId,
+    string Tags,
+    string DisplayCategories,
+    string UpgradeLootName,
+    long? RentalSeconds,
+    long? RentalGames);
+
+public record PlayerLoot(string LootName, string RefId, long Count);
+
+public record QueryResult(Dictionary<string, string[]> QueryToLootNames, long LastUpdate);
+
+public record RecipeList(Recipe[] Recipes, long LastUpdate);
+
+public record Recipe(string RecipeName, string Type, string DisplayCategories, Slot[] Slots, Output[] Outputs, string CrafterName, Metadata Metadata);
+
+public record Metadata(Description[] GuaranteedDescriptions, Description[] BonusDescriptions, bool TooltipsDisabled);
+
+public record Description(string LootName, string[] ChildLootTableNames);
+
+public record Output(string LootName, string QuantityExpression, double Probability, bool AllowDuplicates);
+
+public record Slot(long SlotNumber, string Query, string QuantityExpression);
