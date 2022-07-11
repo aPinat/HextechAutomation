980d7d3369569e45bcb18941694d1cc2ad0a173e
aPinat <apinat@pinat.dev>
2022-07-11 16:21:57 +0200

    match changes in RiotAuth, update packages

--- HextechAutomation/Program.cs
+++ HextechAutomation/Program.cs
@@ -1,12 +1,10 @@
 ﻿using HextechAutomation;
-using RiotAuth;
 using RiotAuth.Clients;
 
 var username = Environment.GetEnvironmentVariable("USERNAME") ?? throw new ArgumentException("USERNAME environment variable not set");
 var password = Environment.GetEnvironmentVariable("PASSWORD") ?? throw new ArgumentException("PASSWORD environment variable not set");
 
-var rso = new RiotSignOn(username, password);
-var leagueClient = new LeagueClient(rso);
+var leagueClient = await LeagueClient.CreateInstanceAsync(username, password);
 var sessionToken = await leagueClient.GetLoginSessionTokenAsync();
 var hextech = await HextechClient.CreateInstanceAsync(sessionToken);
 await hextech.PrintPlayerLootAsync();
